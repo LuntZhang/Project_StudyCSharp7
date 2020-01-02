ffae8bb60bbf106cf17846aa27f3cad1cdf95459
张龙涛 <389815117@qq.com>
2020-01-02 15:59:25 +0800

    --委托

---
+++ Chapter8_Delegates/DelegatesAndEventsSamples/BubbleSorter/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubbleSorter
+{
+    class Employee
+    {
+        public Employee(string name,decimal salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+        public string Name { get; }
+        public decimal Salary { get; }
+        public override string ToString() => $"{Name},{Salary}";
+        public static bool CompareSalary(Employee e1, Employee e2) => e1.Salary < e2.Salary;
+    }
+}
---
+++ Chapter8_Delegates/DelegatesAndEventsSamples/SimpleDelegates/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SimpleDelegates
+{
+    delegate double DoubleOp(double x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 委托数组
+            DoubleOp[] operations =
+            {
+                MathOperations.MultiplyByTwo,
+                MathOperations.Square
+            };
+            // Func<double, double>[] operations =
+            //{
+            //     MathOperations.MultiplyByTwo,
+            //     MathOperations.Square
+            // };
+            for (int i = 0; i < operations.Length; i++)
+            {
+                Console.WriteLine($"Using operations[{i}]:");
+                ProcessAndDisplayNumber(operations[i], 2.0);
+                ProcessAndDisplayNumber(operations[i], 7.90);
+                ProcessAndDisplayNumber(operations[i], 1.414);
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+
+        static void ProcessAndDisplayNumber(DoubleOp action, double value)
+        {
+            double result = action(value);
+            Console.WriteLine($"value is {value},result of operation is {result}");
+            Console.ReadKey();
+        }
+        //static void ProcessAndDisplayNumber(Func<double,double> action, double value)
+        //{
+        //    double result = action(value);
+        //    Console.WriteLine($"value is {value},result of operation is {result}");
+        //    Console.ReadKey();
+        //}
+    }
+}
