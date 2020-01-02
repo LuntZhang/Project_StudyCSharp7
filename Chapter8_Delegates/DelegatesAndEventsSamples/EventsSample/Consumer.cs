585eb30a5e0b24d71fdf4e0e6f67a65b9834a2f8
张龙涛 <389815117@qq.com>
2020-01-02 16:58:57 +0800

    --事件

---
+++ Chapter8_Delegates/DelegatesAndEventsSamples/EventsSample/Consumer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsSample
+{
+    public class Consumer
+    {
+        private string _name;
+
+        public Consumer(string name) => _name = name;
+
+        public void NewCarIsHere(object sender, CarInfoEventArgs e) =>
+          Console.WriteLine($"{_name}: car {e.Car} is new");
+    }
+}
---
+++ Chapter8_Delegates/DelegatesAndEventsSamples/LambdaExpressions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleDemos();
+        }
+
+        static void SimpleDemos()
+        {
+            Console.WriteLine(nameof(SimpleDemos));
+            Func<string, string> oneParm = s => $"change uppercase {s.ToUpper()}";
+            Console.WriteLine(oneParm("test"));
+
+            Func<double, double, double> twoParams = (x, y) => x * y;
+            Console.WriteLine(twoParams(3,2));
+
+            Func<double, double, double> twoParamsWithTypes = (double x, double y) => x * y;
+            Console.WriteLine(twoParamsWithTypes(4, 2));
+
+        }
+
+        static void ClosureWithModification()
+        {
+            Console.WriteLine(nameof(ClosureWithModification));
+            int someVal = 5;
+            Func<int, int> f = x => x + someVal;
+
+            someVal = 7;
+
+            Console.WriteLine(f(3));
+            Console.WriteLine();
+        }
+
+    }
+}
