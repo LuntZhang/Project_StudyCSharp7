dcef235a80260494f7847986dcf9976ca5dbd892
张龙涛 <389815117@qq.com>
2020-01-03 15:24:09 +0800

    --集合

---
+++ Chapter10_Collections/Collections/ListSamples/FindCountry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListSamples
+{
+    class FindCountry
+    {
+        public FindCountry(string country) => _country = country;
+
+        private readonly string _country;
+
+        public bool FindCountryPredicate(Racer racer) => racer?.Country == _country;
+    }
+}
